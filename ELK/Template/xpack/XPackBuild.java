5c7411fa39807bac8adc8c62bfbc7902e985c85a
Fu Xuanming <newsfuxuanming@foxmail.com>
2023-06-26 10:41:13 +0800

    Update XPackBuild.java

--- ELK/Template/xpack/XPackBuild.java
+++ ELK/Template/xpack/XPackBuild.java
@@ -18,12 +18,54 @@ import java.util.jar.*;
 
 import org.elasticsearch.common.*;
 
-
 public class XPackBuild {
+
     public static final XPackBuild CURRENT;
+
+    static {
+        final String shortHash;
+        final String date;
+
+        Path path = getElasticsearchCodebase();
+        if (path.toString().endsWith(".jar")) {
+            try (JarInputStream jar = new JarInputStream(Files.newInputStream(path))) {
+                Manifest manifest = jar.getManifest();
+                shortHash = "Unknown";
+                date = "Unknown";
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            shortHash = "Unknown";
+            date = "Unknown";
+        }
+
+        CURRENT = new XPackBuild(shortHash, date);
+    }
+
+    @SuppressForbidden(reason = "looks up path of xpack.jar directly")
+    static Path getElasticsearchCodebase() {
+        URL url = XPackBuild.class.getProtectionDomain().getCodeSource().getLocation();
+        try {
+            return PathUtils.get(url.toURI());
+        } catch (URISyntaxException bogus) {
+            throw new RuntimeException(bogus);
+        }
+    }
+
     private String shortHash;
     private String date;
 
-    @SuppressForbidden(reason = "looks up path of xpack.jar directly")
-    static Path getElastic
+    XPackBuild(String shortHash, String date) {
+        this.shortHash = shortHash;
+        this.date = date;
+    }
+
+    public String shortHash() {
+        return shortHash;
+    }
+
+    public String date() {
+        return date;
+    }
 }
